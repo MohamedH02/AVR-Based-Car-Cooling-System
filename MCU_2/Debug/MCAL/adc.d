911b0d2cb48a48219b01b3b578831719f1d8c19e
Mohamed Hisham <113701968+MohamedH02@users.noreply.github.com>
2024-08-21 19:43:06 +0300

    Delete MCU_2 directory

--- MCU_2/Debug/MCAL/adc.d
+++
@@ -1,8 +0,0 @@
-MCAL/adc.o MCAL/adc.o: ../MCAL/adc.c ../MCAL/adc.h ../MCAL/..\std_types.h \
-  ../MCAL/..\common_macros.h
-
-../MCAL/adc.h:
-
-../MCAL/..\std_types.h:
-
-../MCAL/..\common_macros.h:
--- MCU_2/Debug/MCAL/pwm_timer0.d
+++
@@ -1,8 +0,0 @@
-MCAL/pwm_timer0.o MCAL/pwm_timer0.o: ../MCAL/pwm_timer0.c \
-  ../MCAL/pwm_timer0.h ../MCAL/..\std_types.h ../MCAL/gpio.h
-
-../MCAL/pwm_timer0.h:
-
-../MCAL/..\std_types.h:
-
-../MCAL/gpio.h:
